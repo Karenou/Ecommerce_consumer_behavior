cb0679f7a24d435b885a13e8c3ac21d3fb200f08
Karenou <karenou0718@gmail.com>
2022-02-18 16:00:25 +0800

    add batch save

--- bmi_prediction/bmi_prediction.py
+++ bmi_prediction/bmi_prediction.py
@@ -1,8 +1,10 @@
+from random import shuffle
 import cv2
 import os
 import time
 import numpy as np
 import pandas as pd
+import math
 
 import tensorflow as tf
 import keras
@@ -34,10 +36,10 @@ def extract_features(x, datagen, model, batch_size, n_images):
     @param batch_size
     @param n_images: number of images
     """
-    print("Extracting features")
+    print("---------Extract features")
     features = np.zeros(shape=(n_images, 2048))
     
-    generator = datagen.flow(x, batch_size=batch_size)
+    generator = datagen.flow(x, batch_size=batch_size, shuffle=False)
     
     total = 0
     left_index = 0
@@ -67,9 +69,9 @@ def read_image(image_path, process_image_path, image_ids, input_shape=(224,224,3
     @param image_ids: a list of image ids that contain human body
     @param input_shape: default input shape of resenet model to resize images
     """
-    print("Reading raw input and processed images")
+    print("---------Read images")
     input_images = []
-    
+    print(image_ids)
     for image_id in image_ids:
         # load raw input image
         raw_input_image = cv2.imread("%s/%d.jpg" % (image_path, image_id))
@@ -103,11 +105,13 @@ if __name__ == "__main__":
     parser.add_argument("--image_path", type=str, help="Base path to save raw input images")
     parser.add_argument("--process_image_path", type=str, help="Base path to save the images of detected faces")
     parser.add_argument("--model_path", type=str, help="Path to load the pretrain model")
+    parser.add_argument("--use_cuda", type=int, default=0, help="which cuda to use")
+    parser.add_argument("--resume_idx", type=int, default=0, help="resume batch idx")
     args = parser.parse_args()
 
     # load image_ids that are detected to contain human body
     human_detect_res = pd.read_csv(args.input_csv)
-    image_ids = human_detect_res[human_detect_res["pred_label"] == 1]["image_id"]
+    image_ids = human_detect_res["image_id"]
 
     image_base_path = args.image_path
     process_image_path = args.process_image_path
@@ -116,28 +120,57 @@ if __name__ == "__main__":
 
     start = time.time()
 
-    # load and preprocess images
-    input_images = read_image(image_base_path, process_image_path, image_ids, input_shape)
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        print("GPU available, using %s" % gpus[args.use_cuda].name)
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(args.use_cuda)
+        tf.config.experimental.set_virtual_device_configuration(
+            gpus[args.use_cuda],
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=10240)]
+        )
+    else:
+        print("GPU not available")
 
-    # extract features using ResNet152 base_model
-    datagen = image.ImageDataGenerator(samplewise_center=True)
+    # load and preprocess images, save result every 10 batches
+    n_batch = math.ceil(len(image_ids) / batch_size)
+    bmi_df = human_detect_res[["image_id", "detect_body"]].copy()
+    bmi_pred = np.zeros(shape=(len(image_ids), 1))
+
+    print("---------Load model")
     base_model = load_base_model(input_shape)
-    features = extract_features(input_images, datagen, base_model, batch_size, len(image_ids))
-
-    # predict bmi using pretrained model
-    print("Predicting BMI")
     model = keras.models.load_model(args.model_path, 
-                                    custom_objects= {'coeff_determination': coeff_determination}
-                                )
+                                            custom_objects= {'coeff_determination': coeff_determination}
+                                        )
 
-    preds = model.predict(features)
-    bmi_df = human_detect_res[human_detect_res["pred_label"] == 1].reset_index(drop=True).copy()
-    bmi_df["pred_bmi"] = preds
+    for i in range(args.resume_idx, n_batch):
+        print("---------Batch %d, Process %d/%d" % (i, i * batch_size, len(image_ids)))
+        if i != n_batch - 1:
+            input_images = read_image(image_base_path, process_image_path, image_ids[i*batch_size:(i+1)*batch_size], input_shape)
+            n_images = len(image_ids[i*batch_size:(i+1)*batch_size])
+        else:
+            input_images = read_image(image_base_path, process_image_path, image_ids[i*batch_size:], input_shape)
+            n_images = len(image_ids[i*batch_size:])
 
-    if not os.path.exists("result"):
-        os.makedirs("result")
+        # extract features using ResNet152 base_model
+        features = base_model.predict(input_images)
 
-    bmi_df.to_csv("result/bmi_pred.csv", index=False)
+        # predict bmi using pretrained model
+        print("---------Predicting BMI")
+        preds = model.predict(features)
+
+        if i != n_batch - 1:
+            bmi_pred[i*batch_size:(i+1)*batch_size] = preds
+        else:
+            bmi_pred[i*batch_size:] = preds
+
+        bmi_df["pred_bmi"] = bmi_pred
+
+        if not os.path.exists("result"):
+            os.makedirs("result")
+
+        if i % 25 == 0:
+            print("---------Save intermediate prediction")
+            bmi_df.to_csv("result/bmi_pred.csv", index=False)
 
     end = time.time()
     print("Total time used: %.2f" % (end-start))
